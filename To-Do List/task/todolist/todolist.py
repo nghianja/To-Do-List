33834854d8c807fdac515d9490312f696e455df7
James Ng <james.ng@projapps.com>
2020-09-26 11:34:53 +0800

    Complete Stage #1

--- To-Do List/task/todolist/todolist.py
+++ To-Do List/task/todolist/todolist.py
@@ -1,1 +1,8 @@
 # Write your code here
+
+if __name__ == "__main__":
+    print("Today:")
+    print("1) Do yoga")
+    print("2) Make breakfast")
+    print("3) Learn basics of SQL")
+    print("4) Learn what is ORM")
